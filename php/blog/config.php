9561d323fb65f3203d6ad646325471d63598bda9
BojanDzogazovic <bdzogazovic@gmail.com>
2019-03-14 15:10:23 +0100

    Delete config.php

--- php/blog/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$connect = mysqli_connect("sql202.epizy.com", "epiz_21987433", "LiwJjSJkAcdS", "epiz_21987433_renegadesblog");
-
-if(mysqli_connect_errno()) {
-    echo "Connection to data base failed: " .mysqli_connect_errno();
-}
-
-?>
